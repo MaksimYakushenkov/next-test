2f027f217c24159f0398a7c966602a71db428a18
Maksim Yakushenkov <breakingwebnews@icloud.com>
2023-01-16 14:21:16 +0300

    'Done'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,31 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if(typeof window !=='undefined') {
+      ym('hit', url);
+    }
+  });
+
   return <>
   <Head>
-        <title>Тест НЕКСТJS</title>
-        <meta name="description" content="Первое некст джиес" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-
-      </Head>
+    <title>Тест НЕКСТJS</title>
+    <meta name="description" content="Первое некст джиес" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="preconnect" href="https://mc.yandex.ru"/>
+    <link rel="icon" href="/favicon.ico" />
+    <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+    <meta property='og:locale' content='ru_RU' />
+  </Head>
+  <YMInitializer
+    accounts={[]} //id счетиков
+    options={{ webvisor: false, defer: true }} // вкл/выкл вебвизор (нагрузка на сервер), defer - откладывем загрузку метрики
+    version='2' // версия счетчика
+  />
   <Component {...pageProps} />
   </>;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
@@ -12,5 +12,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
